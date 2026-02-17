1bc4516e73f0dabe728ceb9616363daaf42b5239
RyumaKazue <ryumaa0426@gmail.com>
2026-02-18 00:19:52 +0900

    useAsyncの使ってみた

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,11 +1,47 @@
-import { TextField, Stack, Link, Typography} from "@mui/material";
+import { TextField, Stack, Link, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
+import { useAsync } from "react-use";
 import { CustomLinkItemBucket } from "../features/customLink/customLinkSchema";
 import { customLinkItemBucket } from "../features/customLink/customLink";
 
 export const App: React.FC = () => {
+    const [query, setQuery] = useState("");
     const [bucket, setBucket] = useState<CustomLinkItemBucket>({});
-    const [itemByGroup, setItemByGroup] = useState<Record<string, React.JSX.Element[]>>({});
+    const state = useAsync(async () => {
+        const groupItems: Record<string, React.JSX.Element[]> = {};
+
+        for (const item of Object.values(bucket)) {
+            if (!item.enabled) {
+                continue;
+            }
+
+            const regex = new RegExp("(\\s|^)" + item.match + "(\\s|$)");
+            if (!regex.test(query)) {
+                continue;
+            }
+
+            groupItems[item.group] = groupItems[item.group] ?? [];
+            if (item.url.includes("%s")) {
+                const keyword = encodeURIComponent(
+                    query.replace(regex, " ").trim()
+                );
+                groupItems[item.group].push(
+                    <Link href={item.url.replace("%s", keyword)} target="_blank">{item.name}</Link>
+                )
+
+            } else {
+                groupItems[item.group].push(
+                    <Stack direction="row" spacing={1} justifyContent="flex-start">
+                        <Link href={item.url} target="_blank">{item.name}</Link>
+                    </Stack>
+                )
+            }
+
+
+        }
+
+        return groupItems;
+    }, [query]);
 
 
     useEffect(() => {
@@ -24,48 +60,15 @@ export const App: React.FC = () => {
                 variant="outlined"
                 placeholder="検索キーワードを入力"
                 onChange={(event) => {
-                    const query = event.target.value;
-
-                    const groupItems: Record<string, React.JSX.Element[]> = {};
-
-                    for(const item of Object.values(bucket)){
-                        if(!item.enabled){
-                            continue;
-                        }
-
-                        const regex = new RegExp("(\\s|^)"+item.match+"(\\s|$)");
-                        if(!regex.test(query)){
-                            continue;
-                        }
-
-                        groupItems[item.group] = groupItems[item.group] ?? [];
-                        if(item.url.includes("%s")){
-                            const keyword = encodeURIComponent(
-                                query.replace(regex, " ").trim()
-                            );
-                            groupItems[item.group].push(
-                                <Link href={item.url.replace("%s", keyword)} target="_blank">{item.name}</Link>
-                            )
-
-                        }else{
-                            groupItems[item.group].push(
-                                <Stack direction="row" spacing={1} justifyContent="flex-start">
-                                    <Link href={item.url} target="_blank">{item.name}</Link>
-                                </Stack>
-                            )
-                        }
-
-
-                    }
-                    setItemByGroup(groupItems);
+                    setQuery(event.target.value);
 
                 }}
             />
 
-            <Stack direction="column" spacing={1} sx={{p: 1,borderTop: 1, borderColor: "divider"}}>
-                {Object.entries(itemByGroup).map(([group, elements]) => (
+            <Stack direction="column" spacing={1} sx={{ p: 1, borderTop: 1, borderColor: "divider" }}>
+                {Object.entries(state.value ?? {}).map(([group, elements]) => (
                     <Stack key={group} direction="column" border={1} borderColor="divider" justifyContent="flex-start" p={1}>
-                        <Typography variant="subtitle2" sx={{p: 1, borderBottom: 1, borderColor: "divider"}}>
+                        <Typography variant="subtitle2" sx={{ p: 1, borderBottom: 1, borderColor: "divider" }}>
                             {group}
                         </Typography>
                         {elements}
